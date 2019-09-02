fd6bd75676edd903bc6bb24fd9490bf34406e177
shy_azusa <shy.azusa@gmail.com>
2019-09-02 11:28:49 +0900

    Make Main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from './Header';
 
+const Main = styled.div`
+  height: calc(100vh - 50px);
+`;
 const App: React.FC = () => {
   return (
     <div>
       <Header />
+      <Main>
+      </Main>
     </div>
   );
 }
