f6023f3e00ed2baa71e70c72447bbd10d8f82d66
shy_azusa <shy.azusa@gmail.com>
2019-09-02 11:18:59 +0900

    Make Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: #8bd1e0;
+`;
+
+const Header: React.FC = () => {
+  return (
+    <Wrapper>
+      header
+    </Wrapper>
+  );
+}
+
+export default Header;
