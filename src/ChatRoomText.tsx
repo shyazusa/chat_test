749a59b0e14cc1c9dbd6e322a77d8528e68c13bc
shy_azusa <shy.azusa@gmail.com>
2019-09-04 11:27:40 +0900

    Change Text form

--- src/ChatRoomText.tsx
+++ src/ChatRoomText.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  background-color: #c5e687;
   flex: 2;
-  background-color: #D3DDE9;
+  display: flex;
 `;
+const TextArea = styled.textarea`
+  font-family: inherit;
+  font-size: 100%;
+  flex: 12;
+`;
+const Btn = styled.button`
+  flex: 1;
+`;
+
 const ChatRoomText: React.FC = () => {
+  const [text, setText] = useState("");
+
   return (
     <Wrapper>
-      chat room text
+      <TextArea
+        placeholder="Type Here"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <Btn
+        type="submit"
+        onClick={() => {
+          console.log("submit!!", text);
+          setText("")
+        }}
+      >
+        Send
+      </Btn>
     </Wrapper>
-  );
-}
+  )
+};
 
 export default ChatRoomText;
