59c8e71f6e5c8511d4d17604af404d2972bbc99e
shy_azusa <shy.azusa@gmail.com>
2019-09-11 10:33:16 +0900

    Add register function in user model

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   EMAIL_FORMAT = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  PASSWORD_FORMAT = /\A[a-zA-Z0-9_-]{9,}\Z/
 
   has_many :rooms, through: :user_rooms
   has_many :msgs
@@ -12,4 +13,35 @@ class User < ApplicationRecord
     format: EMAIL_FORMAT,
     uniqueness: true
 
+  def self.register(name, email, password)
+    errs = User.validate_password(password)
+    if errs.length > 0
+      return Result.new(errs, nil)
+    end
+    hashed_password = Digest::SHA256.hexdigest(password)
+    user = User.new(name: name, email: email, password: hashed_password)
+    if user.save
+      return Result.new([], user)
+    else
+      return Result.new(user.errors.full_messages, user)
+    end
+  end
+
+  private
+  def self.validate_password(password)
+    errs = []
+    if password.blank?
+      errs << "パスワードは必須です"
+      return errs
+    end
+
+    has_char = !password.index(/[a-zA-z]/).nil?
+    has_number = !password.index(/[0-9]/).nil?
+    has_sym = !password.index(/[_-]/).nil?
+
+    if !PASSWORD_FORMAT.match?(password) || !has_char || !has_number || !has_sym
+      errs << "パスワードは9文字以上で，英字，数字，記号(-_)をそれぞれ1つ以上含む必要があります"
+    end
+    return errs
+  end
 end
